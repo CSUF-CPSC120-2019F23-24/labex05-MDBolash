f9550f14c896706ede91eaf2d4a8a6d61173fe67
MD_Bolash <md_bolash@cs104u-1c.acad.fullerton.edu>
2019-09-21 14:30:05 -0700

    fixed username generator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,3 +1,31 @@
-// Random Rectangle
+// This program creates a Random Rectangle
 #include <iostream>
 #include <iomanip>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+
+
+int main()
+{
+
+  //Create max for width and length of rectangle.
+  const int MAX = 40 ;
+
+  //create random numbers
+  srand(time(0)) ;
+  int num;
+  num = rand() % MAX + 1 ;
+
+  std::string rect;
+
+  rect.assign(num, '#') ;
+
+  std::cout << rect << std::endl;
+
+  std::cout << rect << std::endl;
+
+  std::cout << rect << std::endl;
+
+  return 0 ;
+}
